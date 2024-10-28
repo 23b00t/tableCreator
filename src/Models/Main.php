80dd77682905a352b058c4ae231783e39f3e76d8
Daniel Kipp <daniel.kipp@gmail.com>
2024-10-28 14:55:37 +0100

    completed Main.php

--- src/Models/Main.php
+++ src/Models/Main.php
@@ -2,6 +2,9 @@
 
 namespace App\Models;
 
+use App\Core\Db;
+use PDO;
+
 class Main implements IModel
 {
     /**
@@ -23,19 +26,106 @@ class Main implements IModel
         $this->name = $name;
     }
 
+    /**
+     * getAllAsObjects
+     *
+     * @return array
+     */
     public function getAllAsObjects(): array
     {
+        $pdo = Db::getConnection();
+        $sql = 'SELECT * FROM main';
+        $stmt = $pdo->prepare($sql);
+        $stmt->execute();
+        $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        $return = [];
+        foreach ($results as $object) {
+            $return[] = new Main(...$object);
+        }
+
+        return $return;
     }
 
+    /**
+     * deleteObjectById
+     *
+     * @param int $id
+     * @return void
+     */
     public function deleteObjectById(int $id): void
     {
+        $pdo = Db::getConnection();
+        $sql = 'DELETE FROM main WHERE id = ?';
+        $stmt = $pdo->prepare($sql);
+        $stmt->execute([$id]);
     }
 
+    /**
+     * getObjectById
+     *
+     * @param int $id
+     * @return Main
+     */
     public function getObjectById(int $id): Main
     {
+        $pdo = Db::getConnection();
+        $sql = 'SELECT * FROM main WHERE id = ?';
+        $stmt = $pdo->prepare($sql);
+        $stmt->execute([$id]);
+        $result = $stmt->fetch(PDO::FETCH_ASSOC);
+        $return = $result ? new Main(...$result) : null;
+
+        return $return;
     }
 
+    /**
+     * update
+     *
+     * @return void
+     */
     public function update(): void
     {
+        $pdo = Db::getConnection();
+        $sql = 'UPDATE main SET name = ? WHERE id = ?';
+        $stmt = $pdo->prepare($sql);
+        $stmt->execute(
+            [$this->name, $this->id]
+        );
+    }
+
+    /**
+     * insert
+     *
+     * @param string $name
+     * @return Main
+     */
+    public function insert(string $name): Main
+    {
+        $pdo = Db::getConnection();
+        $sql = 'INSERT INTO employee VALUES(NULL, ?)';
+        $stmt = $pdo->prepare($sql);
+        $stmt->execute([$name]);
+        $id = $pdo->lastInsertId();
+        return new Main($id, $name);
+    }
+
+    /**
+     * getId
+     *
+     * @return int|null
+     */
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    /**
+     * getName
+     *
+     * @return string|null
+     */
+    public function getName(): ?string
+    {
+        return $this->name;
     }
 }
