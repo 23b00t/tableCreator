96e7c6402a35ee6cdd79523e67b2b9c8a0bcd368
Daniel Kipp <daniel.kipp@gmail.com>
2024-10-28 14:23:52 +0100

    created model interface

---
+++ src/Models/IModel.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Models;
+
+/**
+ * IModel
+ * Interface for basic methods of Models
+ * @method insert
+ */
+interface IModel
+{
+    /**
+     * getAllAsObjects
+     *
+     * @return array
+     */
+    public function getAllAsObjects(): array;
+
+    /**
+     * deleteObjectById
+     *
+     * @param int $id
+     * @return void
+     */
+    public function deleteObjectById(int $id): void;
+
+    /**
+     * getObjectById
+     *
+     * @param int $id
+     * @return Object
+     */
+    public function getObjectById(int $id): object;
+
+    /**
+     * update
+     *
+     * @return void
+     */
+    public function update(): void;
+}
