f1d08ddec15585791101a77fbaa4e700beba0b33
Daniel Kipp <daniel.kipp@gmail.com>
2024-10-28 14:12:52 +0100

    created index.php

---
+++ public/index.php
@@ -0,0 +1,36 @@
+<?php
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+/**
+ * @var string $area (Controller Name)
+ */
+$area = $_REQUEST['area'] ?? 'main';
+
+/**
+ * @var string $action (Controller action)
+ * showTable as default action
+ */
+$action = $_REQUEST['action'] ?? 'showTable';
+
+/** Build Action Controller Name from $action */
+$controllerName = ucfirst($action) . 'Controller';
+
+/**
+ * Determine request method (POST or GET) and securely pass the corresponding
+ * data ($_POST or $_GET) to the controller, ensuring proper handling of input.
+ */
+$data = $_SERVER['REQUEST_METHOD'] === 'POST' ? $_POST : $_GET;
+
+/**
+ * Invoke the controller and extract variables from the returned array
+ */
+$controller = new $controllerName($data);
+$array = $controller->invoke();
+extract($array);
+
+/** After calling ShowFormController set the action depending on usecase - update or insert */
+$action = $controllerName === 'ShowFormController' ? $controller->getAction() : $action;
+
+/** Include requested view */
+include __DIR__ . '/views/application.html.php';
