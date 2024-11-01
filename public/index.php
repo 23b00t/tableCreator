7f1bed8928648afc6569db36faa84a7488db8aa5
Daniel Kipp <daniel.kipp@gmail.com>
2024-11-01 10:20:12 +0100

    added basic error handling

--- public/index.php
+++ public/index.php
@@ -1,38 +1,42 @@
 <?php
 
-include_once __DIR__ . '/../config.php';
+try {
+    include_once __DIR__ . '/../config.php';
 
-require_once __DIR__ . '/../vendor/autoload.php';
+    require_once __DIR__ . '/../vendor/autoload.php';
 
-/**
- * @var string $area (Controller Name)
- */
-$area = $_REQUEST['area'] ?? 'dataset';
+    /**
+     * @var string $area (Controller Name)
+     */
+    $area = $_REQUEST['area'] ?? 'dataset';
 
-/**
- * @var string $action (Controller action)
- * showTable as default action
- */
-$action = $_REQUEST['action'] ?? 'showTable';
+    /**
+     * @var string $action (Controller action)
+     * showTable as default action
+     */
+    $action = $_REQUEST['action'] ?? 'showTable';
 
-/** Build Action Controller Name from $action */
-$controllerName = 'App\\Controllers\\' . ucfirst($action) . 'Controller';
+    /** Build Action Controller Name from $action */
+    $controllerName = 'App\\Controllers\\' . ucfirst($action) . 'Controller';
 
-/**
- * Determine request method (POST or GET) and securely pass the corresponding
- * data ($_POST or $_GET) to the controller, ensuring proper handling of input.
- */
-$data = $_SERVER['REQUEST_METHOD'] === 'POST' ? $_POST : $_GET;
+    /**
+     * Determine request method (POST or GET) and securely pass the corresponding
+     * data ($_POST or $_GET) to the controller, ensuring proper handling of input.
+     */
+    $data = $_SERVER['REQUEST_METHOD'] === 'POST' ? $_POST : $_GET;
 
-/**
- * Invoke the controller and extract variables from the returned array
- */
-$controller = new $controllerName($data);
-$array = $controller->invoke();
-extract($array);
+    /**
+     * Invoke the controller and extract variables from the returned array
+     */
+    $controller = new $controllerName($data);
+    $array = $controller->invoke();
+    extract($array);
 
-/** After calling ShowFormController set the action depending on usecase - update or insert */
-$action = $controllerName === 'App\Controllers\ShowFormController' ? $controller->getAction() : $action;
-
-/** Include requested view */
-include __DIR__ . '/../src/Views/application.php';
+    /** After calling ShowFormController set the action depending on usecase - update or insert */
+    $action = $controllerName === 'App\Controllers\ShowFormController' ? $controller->getAction() : $action;
+} catch (Exception $e) {
+    $errorMsg = $e;
+} finally {
+    /** Include requested view */
+    include __DIR__ . '/../src/Views/application.php';
+}
--- src/Core/Db.php
+++ src/Core/Db.php
@@ -2,6 +2,8 @@
 
 namespace App\Core;
 
+use DateTime;
+use Exception;
 use PDOException;
 use PDO;
 
@@ -29,7 +31,9 @@ class Db
             try {
                 self::$dbh = new PDO(DB_DSN, DB_USER, DB_PASSWD);
             } catch (PDOException $e) {
-                echo 'Connection failed: ' . $e->getMessage();
+                $timestamp = (new DateTime())->format('Y-m-d H:i:s.u ');
+                file_put_contents(LOG_PATH, $timestamp . $e->getMessage(), FILE_APPEND);
+                throw new Exception("Es ist ein Fehler aufgetreten");
             }
         }
         return self::$dbh;
